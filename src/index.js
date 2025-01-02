c95a637ee64d8071f49821a91314b165076f7447
Harshit Goel <harshitgoel692@gmail.com>
2025-01-02 12:12:44 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-console.log(`${process.env.PORT}`);
 
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error occured before listening:", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("Connection to DB is Failed!!", error);
+    
+})
