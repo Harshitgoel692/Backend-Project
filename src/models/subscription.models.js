cb8d98f4b939c58d96badb2dfbc5fd1156ace363
Harshit Goel <harshitgoel692@gmail.com>
2025-01-15 14:18:21 +0530

    add subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,7 @@
+import mongoose, Schema from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+},{timestamps: true});
+
+export const Subscription= mongoose.model("Subscription", subscriptionSchema)
